e2404cee8c9a0c0f7eab07a58fd544bb51b65422
desouzagabriela21 <desouzagabriela28@gmail.com>
2025-07-14 20:54:10 -0300

    Correção de erros

--- JogoRPG.java
+++ JogoRPG.java
@@ -18,98 +18,119 @@ public class JogoRPG {
         int opcao;
         boolean reiniciar = true;
 
-        while (reiniciar == true){
-        while (personagem1.vida > 0 && personagem2.vida > 0){
-            System.out.printf("\n\nÉ a vez do jogador %d!", jogador);
-            System.out.printf("\n1 - Atacar\n2 - Ataque especial\n3 - Elixir de vida\nOpçao: ");
+        while (reiniciar == true) {
+            while (personagem1.vida > 0 && personagem2.vida > 0) {
+                System.out.printf("\n\nÉ a vez do jogador %d!", jogador);
+                System.out.printf("\n1 - Atacar\n2 - Ataque especial\n3 - Elixir de vida\nOpçao: ");
 
-            do {
-                opcao = scanner.nextInt();
-                
-                switch(opcao){
+                do {
+                    opcao = scanner.nextInt();
+                    
+                    switch(opcao) {
 
-                case 1: if (jogador == 1){
-                    personagem1.atacar();
-                    personagem2.receberDano();
-                    } else {
-                        personagem2.atacar();
-                        personagem1.receberDano();
-                    } break;
-
-                case 2: if (jogador == 1){
-                    personagem1.usarHabilidadeEspecial();
-                        if (personagem1.vida >= 85){
-                        personagem2.receberDanoAtaqueEspecial();
-                        }
-                    } else {
-                        personagem2.usarHabilidadeEspecial();
-                        if (personagem2.vida >= 85){
-                        personagem1.receberDanoAtaqueEspecial();
-                        }
-                    } break;
-
-                case 3: 
-                if (jogador == 1) {
-                    if (personagem1.vida < 90 || < 90){
-                        personagem1.vida += 10; 
-                        System.out.printf("Vida +10\nVida atual: %d\n", personagem1.vida);
-                        if (personagem1.classe.equals("Mago")) {
-                            if (personagem1.mana < 90) {
-                                personagem1.mana += 10;
-                                System.out.printf("Mana +10\nMana atual: %d\n", personagem1.mana);
+                        case 1:
+                            if (jogador == 1) {
+                                personagem1.atacar();
+                                personagem2.receberDano();
+                            } else {
+                                personagem2.atacar();
+                                personagem1.receberDano();
+                            }
+                            break;
+                        
+                        case 2:
+                            if (jogador == 1) {
+                                personagem1.usarHabilidadeEspecial();
+                                if (personagem1.classe.equals("Mago") && personagem1.mana >= 85 && personagem1.vida >= 85) {
+                                    personagem2.receberDanoAtaqueEspecial();
+                                } else if ((personagem1.classe.equals("Guerreiro") || personagem1.classe.equals("Arqueiro")) && personagem1.vida >= 85) {
+                                    personagem2.receberDanoAtaqueEspecial();
+                                }
+                            } else {
+                                personagem2.usarHabilidadeEspecial();
+                                if (personagem2.classe.equals("Mago") && personagem2.mana >= 85 && personagem2.vida >= 85) {
+                                    personagem1.receberDanoAtaqueEspecial();
+                                } else if ((personagem2.classe.equals("Guerreiro") || personagem2.classe.equals("Arqueiro")) && personagem2.vida >= 85) {
+                                    personagem1.receberDanoAtaqueEspecial();
                                 }
                             }
-                    } else {
-                        System.out.println("Elixir indisponível!");
-                        }
-                    } else {
-                        if (personagem2.vida < 90) {
-                            personagem2.vida += 10; 
-                            System.out.printf("Vida +10\nVida atual: %d\n", personagem2.vida);
-                            if (personagem2.classe.equals("Mago")) {
-                                if (personagem2.mana < 90) {
-                                    personagem2.mana += 10;
-                                    System.out.printf("Mana +10\nMana atual: %d\n", personagem2.mana);
+                            break;
+
+                        case 3:
+                            if (jogador == 1) {
+                                if (personagem1.vida < 90) {
+                                    personagem1.vida += 10; 
+                                    System.out.printf("Vida +10\nVida atual: %d\n", personagem1.vida);
+                                    if (personagem1.classe.equals("Mago")) {
+                                        if (personagem1.mana < 90) {
+                                            personagem1.mana += 10;
+                                            System.out.printf("Mana +10\nMana atual: %d\n", personagem1.mana);
+                                        }
                                     }
-                                } 
-                        } else {
-                            System.out.println("Elixir indisponível!"); 
+                                } else {
+                                    System.out.println("Elixir indisponível!");
+                                }
+                            } else {
+                                if (personagem2.vida < 90) {
+                                    personagem2.vida += 10; 
+                                    System.out.printf("Vida +10\nVida atual: %d\n", personagem2.vida);
+                                    if (personagem2.classe.equals("Mago")) {
+                                        if (personagem2.mana < 90) {
+                                            personagem2.mana += 10;
+                                            System.out.printf("Mana +10\nMana atual: %d\n", personagem2.mana);
+                                        }
+                                    } 
+                                } else {
+                                    System.out.println("Elixir indisponível!"); 
+                                }
                             }
-                    } break;
-                    
-                default:
-                    System.out.printf("Opçao inválida!\nOpçao: "); break;
+                            break;
+                        
+                        default:
+                            System.out.printf("Opçao inválida!\nOpçao: ");
+                            break;
 
-            } } while (opcao != 1 && opcao != 2 && opcao != 3);
+                    }
+                } while (opcao != 1 && opcao != 2 && opcao != 3);
 
-            if (jogador == 1){
-                jogador = 2;
-            } else {
-                jogador = 1;
+                if (jogador == 1) {
+                    jogador = 2;
+                } else {
+                    jogador = 1;
+                }
             }
-        }
 
-        if (personagem1.vida <= 0){
-            System.out.printf("\nO jogador 2 venceu!");
-            personagem2.desenharPersonagem();
-            personagem2.subirNivel();
-        } 
-        if (personagem2.vida <= 0){
-            System.out.printf("\nO jogador 1 venceu!");
-            personagem1.desenharPersonagem();
-            personagem1.subirNivel();
-        } 
+            if (personagem1.vida <= 0) {
+                System.out.printf("\nO jogador 2 venceu!");
+                personagem2.desenharPersonagem();
+                personagem2.subirNivel();
+            } 
+            if (personagem2.vida <= 0) {
+                System.out.printf("\nO jogador 1 venceu!");
+                personagem1.desenharPersonagem();
+                personagem1.subirNivel();
+            } 
 
-        System.out.print("Deseja reiniciar o jogo?\n1 - Sim\n2 - Nao\nOpçao:");
+            System.out.print("Deseja reiniciar o jogo?\n1 - Sim\n2 - Nao\nOpçao:");
             opcao = scanner.nextInt();
-            if(opcao == 2){
-            reiniciar = false; 
-            System.out.println("\n======FIM DE JOGO======");
-            break;
-            } if (opcao == 1)
-            reiniciar = true;
-            personagem1.vida = 100;
-            personagem2.vida = 100;
+            if (opcao == 2) {
+                reiniciar = false; 
+                System.out.println("\n======FIM DE JOGO======");
+                break;
+            }
+            
+            if (opcao == 1) {
+                reiniciar = true;
+                personagem1.vida = 100;
+                personagem2.vida = 100;
+                if (personagem1.classe.equals("Mago")) {
+                    personagem1.mana = 100;
+                }
+                if (personagem2.classe.equals("Mago")) {
+                    personagem2.mana = 100;
+                }
+            }
+
         }
     }
 }
--- RPG.java
+++ RPG.java
@@ -11,12 +11,12 @@ public class RPG {
 
     public void mostrarStatus(){
         if (classe.equals("Mago")){
-        System.out.printf("\nStatus: ");
-        System.out.printf("\nNome: %s|Classe: %s|Nível: %d|Vida: %d|Força: %d |Mana: %d|\n", nome, classe, nivel, vida, forca, mana);
+            System.out.printf("\nStatus: ");
+            System.out.printf("\nNome: %s|Classe: %s|Nível: %d|Vida: %d|Força: %d |Mana: %d|\n", nome, classe, nivel, vida, forca, mana);
         } else {
-        System.out.printf("\nStatus: ");
-        System.out.printf("\nNome: %s|Classe: %s|Nível: %d|Vida: %d|Força: %d|\n", nome, classe, nivel, vida, forca);        }
-
+            System.out.printf("\nStatus: ");
+            System.out.printf("\nNome: %s|Classe: %s|Nível: %d|Vida: %d|Força: %d|\n", nome, classe, nivel, vida, forca);
+        }
     }
 
     public void atacar(){
@@ -31,8 +31,8 @@ public class RPG {
         System.out.printf("\nO %s %s foi atacado por seu oponente!", classe, nome);
         vida -= (int)(Math.random() * 31);
         System.out.printf("\nVida: %d",vida);
-        if(vida == 0){
-            System.out.printf("\n%d morreu!\nFim de jogo!");
+        if(vida <= 0){
+            System.out.printf("\n%s morreu!\nFim de jogo!", nome);
         }
     }
 
@@ -49,27 +49,27 @@ public class RPG {
     public void usarHabilidadeEspecial(){
         if(classe.equals("Guerreiro")){
             if (vida >= 85){
-            System.out.printf("\nO %s %s atacou seu oponente COM SEU SUPER MACHADO!", classe, nome);
-        } else {
-            System.out.printf("\nO %s %s não tem força para um ataque especial!", classe, nome);
-        } 
+                System.out.printf("\nO %s %s atacou seu oponente COM SEU SUPER MACHADO!", classe, nome);
+            } else {
+                System.out.printf("\nO %s %s não tem força para um ataque especial!", classe, nome);
+            } 
         }
         if(classe.equals("Arqueiro")){
             if (vida >= 85){
-            System.out.printf("\nO %s %s atacou seu oponente COM SUA SUPER FLECHA!", classe, nome);
-        } else {
-            System.out.printf("\nO %s %s não tem força para um ataque especial!", classe, nome);
-        } 
+                System.out.printf("\nO %s %s atacou seu oponente COM SUA SUPER FLECHA!", classe, nome);
+            } else {
+                System.out.printf("\nO %s %s não tem força para um ataque especial!", classe, nome);
+            } 
         }
         if(classe.equals("Mago")){
             if (mana >= 85 && vida >= 85){
-            System.out.printf("\nO %s %s atacou seu oponente COM SEU SUPER FEITIÇO!", classe, nome);
-            mana -= (int)(Math.random() * 31);
-            mana -= 15;
-            System.out.printf("\nMana: %d", mana);
-        } else {
-            System.out.printf("\nO %s %s não tem força para um ataque especial!", classe, nome);
-        } 
+                System.out.printf("\nO %s %s atacou seu oponente COM SEU SUPER FEITIÇO!", classe, nome);
+                mana -= (int)(Math.random() * 31);
+                mana -= 15;
+                System.out.printf("\nMana: %d", mana);
+            } else {
+                System.out.printf("\nO %s %s não tem força para um ataque especial!", classe, nome);
+            } 
         }    
     }
     
@@ -91,26 +91,26 @@ public class RPG {
     }
 
     public void desenharPersonagem(){
-    if(classe.equals("Guerreiro")){
-        System.out.println("\n   O   /\\");
-        System.out.println("  /|\\ /  \\");
-        System.out.println("   |  \\__/ ");
-        System.out.println("  / \\     ");
-    }
-    if (classe.equals("Mago")){
-        System.out.println("\n   ^");
-        System.out.println("  /_\\");
-        System.out.println("   O");
-        System.out.println("  /|\\");
-        System.out.println("   |");
-        System.out.println("  / \\");
-    }
-    if (classe.equals("Arqueiro")){
-        System.out.println("\n   O");
-        System.out.println("  /|\\ )>");
-        System.out.println("   |   ");
-        System.out.println("  / \\");
-    }
+        if(classe.equals("Guerreiro")){
+            System.out.println("\n   O   /\\");
+            System.out.println("  /|\\ /  \\");
+            System.out.println("   |  \\__/ ");
+            System.out.println("  / \\     ");
+        }
+        if (classe.equals("Mago")){
+            System.out.println("\n   ^");
+            System.out.println("  /_\\");
+            System.out.println("   O");
+            System.out.println("  /|\\");
+            System.out.println("   |");
+            System.out.println("  / \\");
+        }
+        if (classe.equals("Arqueiro")){
+            System.out.println("\n   O");
+            System.out.println("  /|\\ )>");
+            System.out.println("   |   ");
+            System.out.println("  / \\");
+        }
     }
 
     public void criarPersonagem(){
@@ -126,30 +126,32 @@ public class RPG {
             opcao = scanner.nextInt();
 
             switch(opcao){
-            case 1:
-                classe = "Guerreiro";
-                forca = 50;
-                System.out.printf("\nHerói criado: %s (%s)\n", nome, classe);
-                mostrarStatus(); break;
-            
-            case 2:
-                classe = "Mago";
-                mana = 100;
-                forca = 30;
-                mostrarStatus(); break;
+                case 1:
+                    classe = "Guerreiro";
+                    forca = 50;
+                    System.out.printf("\nHerói criado: %s (%s)\n", nome, classe);
+                    mostrarStatus();
+                    break;
+                
+                case 2:
+                    classe = "Mago";
+                    mana = 100;
+                    forca = 30;
+                    System.out.printf("\nHerói criado: %s (%s)\n", nome, classe);
+                    mostrarStatus();
+                    break;
 
-            case 3:
-                classe = "Arqueiro";
-                forca = 40;
-                System.out.printf("\nHerói criado: %s (%s)\n", nome, classe);
-                mostrarStatus(); break;
+                case 3:
+                    classe = "Arqueiro";
+                    forca = 40;
+                    System.out.printf("\nHerói criado: %s (%s)\n", nome, classe);
+                    mostrarStatus();
+                    break;
 
-            default: 
-            System.out.println("Opção inválida!"); break;
-
+                default: 
+                    System.out.println("Opção inválida!"); 
+                    break;
             }
         } while (opcao != 1 && opcao != 2 && opcao != 3);
     }
-
 }
-
